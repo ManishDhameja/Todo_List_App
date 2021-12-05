da9874de4e7c591e8309482e597cd4a027a86f9c
ManishDhameja <manishdhameja86@gmail.com>
2021-12-05 17:41:20 +0530

    Full app deployed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './MyComponent/todos/todos.component';
+import { TodoItemComponent } from './MyComponent/todo-item/todo-item.component';
+import { AddtodoComponent } from './MyComponent/addtodo/addtodo.component';
+import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './MyComponent/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    AddtodoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
